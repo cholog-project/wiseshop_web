7623a6cb0642f84e60341dc9d309a5e3c75c6715
gogo1414 <wnstn6945@gmail.com>
2025-02-26 16:57:18 +0900

    fix: baseUrl 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://api.wiseshop.kro.kr/api/v1',
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://api.wiseshop.kro.kr/api/v1',
+  // baseURL: 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
